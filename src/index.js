47fa1e13ecd9e0fc0b5aecb5415bafd0016b8b8c
boyka9x <huanzoyza@gmail.com>
2021-11-26 15:14:18 +0700

    test

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(morgan('combined'));
 // Template engine
 app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
-                  app.set("views", path.join(__dirname, 'resources\\views'));
+app.set('views', path.join(__dirname, 'resources\\views'));
 
 // Route init
 route(app);
